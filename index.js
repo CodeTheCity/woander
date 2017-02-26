28967466922b8b226e95ac60435988e410eb4d6d
Luke Fisher <mail@lukefisher.me>
2017-02-26 13:33:19 +0000

    Link location attachment to processing

--- index.js
+++ index.js
@@ -83,19 +83,21 @@ app.post('/fb_webhook', function (req, res) {
           id: messages[m].sender.id,
           text: messages[m].message.text
         });
-      } else if(messages[m].message.attachments){
+      } else if(messages[m].messeage && messages[m].message.attachments){
         var attachments = messages[m].message.attachments;
         attachments.forEach(function(item) {
           var reply;
           switch(item.type){
              case 'location':
-              // TODO: Use the location
-              var mess = {
+              // Use the location
+              fb_parser.run({
                 id: messages[m].sender.id,
-                location: item.payload.coordinates
-              };
-              reply = messenger.createMessage("We know where you live.... lat:"+item.payload.coordinates.lat+" long:"+item.payload.coordinates.long);
-              messenger.sendMessage(recipient,reply);
+                location: {
+                  lat: item.payload.coordinates.lat,
+                  lng: item.payload.coordinates.long
+                }
+              });
+
               break;
             default:
               reply = messenger.createMessage("We currently cannot handle this attachment, sorry.");
