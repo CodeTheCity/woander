7b2d4151f74e60114a6e4f09d807a8966b757359
Fraser <fraser.barclay@hotmail.co.uk>
2017-02-26 10:29:26 +0000

    Returns array of place information based on input query and location

--- information.js
+++ information.js
@@ -1,5 +1,46 @@
+var async = require('async');
+
 var config = require('./config.json');
 
-var googleMapsClient = require('@google/maps').createClient({
-  key: config.google_map_api_key
+var key = config.google_map_api_key;
+
+var googleMaps = require('@google/maps').createClient({
+    key: key
+});
+
+function getDetails(query, long, lat, callback) {
+    googleMaps.places({
+        query: query,
+        location: [long, lat],
+        //radius: radius
+    }, function (err, response) {
+        if (!err) {
+            var results = response.json.results;
+
+            async.map(results, function (result, cb) {
+                getOpeningTimes(result.place_id, function (data) {
+                    cb(null, { address: data });
+                });
+            }, function (err, result) {
+                callback(result);
+            });
+        }
+    });
+
+}
+
+function getOpeningTimes(id, callback) {
+    googleMaps.place({
+        placeid: id
+    }, function (err, response) {
+        if (!err) {
+            callback(response.json.result);//.address_components); //.opening_hours.periods);
+        }
+    });
+}
+
+
+//example call
+getDetails("starbucks", 57.1660063, -2.1054137, function (data) {
+    console.log(data);
 });
